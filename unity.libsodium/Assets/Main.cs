0a629906c9207b7059a1bd7390a507dd5ae696c5
netpyoung <netpyoung@gmail.com>
2021-10-23 12:20:09 +0900

    update scene for android build test

--- unity.libsodium/Assets/Main.cs
+++ unity.libsodium/Assets/Main.cs
@@ -1,9 +1,14 @@
 ﻿using System.Text;
 using unity.libsodium;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Main : MonoBehaviour
 {
+    public Text txt_origin;
+    public Text txt_encrypted;
+    public Text txt_decrypted;
+
     private void Start()
     {
         int x = NativeLibsodium.sodium_init();
@@ -21,8 +26,16 @@ public class Main : MonoBehaviour
         //decrypt it
         byte[] decrypted = StreamEncryption.DecryptChaCha20(encrypted, nonce, key);
 
-        Debug.Log(MESSAGE);
-        Debug.Log(Encoding.UTF8.GetString(encrypted));
-        Debug.Log(Encoding.UTF8.GetString(decrypted));
+        string str_origin = MESSAGE;
+        string str_encrypted = Encoding.UTF8.GetString(encrypted);
+        string str_decrypted = Encoding.UTF8.GetString(decrypted);
+
+        Debug.Log(str_origin);
+        Debug.Log(str_encrypted);
+        Debug.Log(str_decrypted);
+
+        txt_origin.text = str_origin;
+        txt_encrypted.text = str_encrypted;
+        txt_decrypted.text = str_decrypted;
     }
 }
