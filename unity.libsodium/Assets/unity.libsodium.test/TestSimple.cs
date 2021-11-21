bade42fff6d292a5ed1d9457d0d8058976204205
netpyoung <netpyoung@gmail.com>
2021-11-21 22:14:31 +0900

    test: add TestChaCha20

---
+++ unity.libsodium/Assets/unity.libsodium.test/TestSimple.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace unity.libsodium.test
+{
+    public class TestSimple
+    {
+        [Test]
+        public void TestChaCha20()
+        {
+            int x = NativeLibsodium.sodium_init();
+            Assert.True(x == 0 || x == 1);
+
+            const string MESSAGE = "Test message to encrypt";
+            byte[] nonce = StreamEncryption.GenerateNonceChaCha20();
+            byte[] key = StreamEncryption.GenerateKey();
+
+            //encrypt it
+            byte[] encrypted = StreamEncryption.EncryptChaCha20(MESSAGE, nonce, key);
+
+
+            //decrypt it
+            byte[] decrypted = StreamEncryption.DecryptChaCha20(encrypted, nonce, key);
+
+            Assert.AreEqual(MESSAGE, decrypted);
+        }
+
+    }
+}
