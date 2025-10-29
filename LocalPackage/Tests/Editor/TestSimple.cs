49c755f665de7fdff69131f6b370a4389998368c
netpyoung <netpyoung@gmail.com>
2025-10-29 12:46:17 +0900

    LocalPackage/Tests/unity.libsodium.test/ => LocalPackage/Tests/Editor/

--- LocalPackage/Tests/Editor/TestSimple.cs
+++ LocalPackage/Tests/Editor/TestSimple.cs
@@ -4,7 +4,7 @@ using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 
-namespace unity.libsodium.test
+namespace unity.libsodium.EditorTests
 {
     public class TestSimple
     {
